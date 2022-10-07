192719eb6d8016b1cbce74b0065dfc8f95603a09
Iconians <claytoncripe@gmail.com>
2022-10-06 17:17:01 -0700

    inital version complete

--- src/Componets/LandingPage/LandingPage.jsx
+++ src/Componets/LandingPage/LandingPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import InputBase from "../InputBase/InputBase";
 import VideoComponet from "../VideoComponet/VideoComponet";
+import { Helmet } from "react-helmet"
 
 class LandingPage extends React.Component {
 
   render() {
     return(
       <div className="parent-div">
-         <section>
+        <section>
           <div className="video-div">
             <VideoComponet />
             <InputBase />
@@ -44,8 +45,8 @@ class LandingPage extends React.Component {
               sale so that you can utilize them and give your customers a better shopping experience.
             </p>
           </div>
-         </section>
-         <section>
+        </section>
+        <section>
           <div className="results-div">
             <div>
               <p>
@@ -69,8 +70,59 @@ class LandingPage extends React.Component {
               <li>Register your payment portal if you don't already have one.</li>
             </ul>
           </div>
-         </section>
-         
+        </section>
+        <section>
+          <div>
+            <p>What do I get?</p>
+            <div className="chart-grid">
+
+            </div>
+            <InputBase />
+          </div>
+          <div>
+            <p>how is it possible</p>
+            <p>By maximizing the teck that everyone carries</p>
+          </div>
+          <div>
+            <div>
+              <p>
+                Why can't I just hire someone to create the same app for me?
+              </p>
+              <p>
+                Even if the only part of the system was the app it would cost you over 150K just to make the basic version and you risk loosing
+                the money invested if it doesn't go well.
+              </p>
+            </div>
+            <div>
+              <p>
+                How much to buy your app for my company?
+              </p>
+              <p>
+                If you want a cash price it's 225k-350k per license
+              </p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <p>
+              What if I waist 3 months and don't make more money?
+            </p>
+            <p>
+              then you have paid nothing. <br />
+              But what if happens if you get it and your competitors do? <br />
+              We carry all the risks and setup costs. <br />
+              If it is we keep working together. <br />
+              If not, I hope we part as friends.
+            </p>
+          </div>
+          <div>
+          <div className="meetings-iframe-container" data-src="https://meetings.hubspot.com/philip-cutting/store-direct?embed=true"></div>
+          <Helmet>
+            <script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script>
+          </Helmet>   
+          </div>
+        </section>
         </div>
     )
   }
